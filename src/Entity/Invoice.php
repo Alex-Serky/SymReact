feab147d87f1f590bf8c40c235dc04a13d2d3da2
Alex Serky <aserky@gmail.com>
2020-01-21 18:50:22 +0100

    Installation de React et de gestion des customers et invoices

--- src/Entity/Invoice.php
+++ src/Entity/Invoice.php
@@ -33,7 +33,7 @@ use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
  *       }
  *    },
  *      attributes = {
- *          "pagination_enabled" = true,
+ *          "pagination_enabled" = false,
  *          "pagination_items_per_page" = 20,
  *          "order": {"amount":"desc"}
  *      },
